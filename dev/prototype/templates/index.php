ec8fc7364ff99c5d9fdf36302f0824581e640f0c
Christopher Hallahan <challahan@gmail.com>
2016-08-04 14:28:02 -0400

    Initial templating files
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

---
+++ dev/prototype/templates/index.php
@@ -0,0 +1,7 @@
+<!-- Include Header.php -->
+  <?php include("../patterns/partials/header.html") ?>
+
+  Hi I'm the content
+
+  <!-- Include Footer.html -->
+  <?php include("../patterns/partials/footer.html") ?>
