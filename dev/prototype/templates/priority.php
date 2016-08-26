a5b23c875927e7c771e8e99578e6d96982a078f7
Christopher Hallahan <challahan@gmail.com>
2016-08-26 13:11:45 -0400

    Added styling to teaser elements and animation to home page
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/prototype/templates/priority.php
+++ dev/prototype/templates/priority.php
@@ -4,10 +4,17 @@
 <div class="section-default">
   <?php include("../patterns/partials/priority-header.php") ?>
     <div class="inner-wrapper">
-      <?php include("../patterns/partials/priority-body.html") ?>
-      <?php include("../patterns/partials/story-teaser.html") ?>
-      <?php include("../patterns/partials/gallery-teaser.html") ?>
-      <?php include("../patterns/partials/video-teaser.html") ?>
+      <p class="priority-introduction">Provide an inclusive and engaged living-learning environment where all students thrive and graduate as informed citizens committed to a life of impact.</p>
+        <div class="priority-content">
+          <?php include("../patterns/partials/priority-body.html") ?>
+          <section class="priority-media">
+            <?php include("../patterns/partials/story-teaser.html") ?>
+            <div class="priority-media__gallery">
+              <?php include("../patterns/partials/gallery-teaser.html") ?>
+              <?php include("../patterns/partials/video-teaser.html") ?>
+            </div>
+          </section>
+        </div>
     </div>
 </div>
   <!-- Include Footer.html -->
