9c403750f2d60ed6b144300fc680e83a388fbaeb
Christopher Hallahan <challahan@gmail.com>
2016-08-31 16:47:36 -0400

    Sync load google fonts
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/assets/js/script.js
+++ dev/assets/js/script.js
@@ -1731,3 +1731,18 @@ var SidebarMenuEffects = (function() {
   init();
 
 })();
+
+
+// Load Google Fonts
+
+WebFontConfig = {
+    google: { families: [ 'Roboto+Slab:700' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
