93312b8dbe2224c8e0cbdc5c91d86902b29e63fc
Christopher Hallahan <challahan@gmail.com>
2016-08-31 16:36:10 -0400

    Async loading for scripts
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/prototype/patterns/partials/header.php
+++ dev/prototype/patterns/partials/header.php
@@ -13,6 +13,31 @@
     <style type="text/css">
       <?php include("../patterns/partials/inline.html") ?>
     </style>
+    <script async src="../../assets/js/picturefill.min.js"></script>
+    <script>
+  /*! loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. (Based on http://goo.gl/REQGQ by Paul Irish). Licensed MIT */
+  (function( w ){
+  	var loadJS = function( src, cb ){
+  		"use strict";
+  		var ref = w.document.getElementsByTagName( "script" )[ 0 ];
+  		var script = w.document.createElement( "script" );
+  		script.src = src;
+  		script.async = true;
+  		ref.parentNode.insertBefore( script, ref );
+  		if (cb && typeof(cb) === "function") {
+  			script.onload = cb;
+  		}
+  		return script;
+  	};
+  	// commonjs
+  	if( typeof module !== "undefined" ){
+  		module.exports = loadJS;
+  	}
+  	else {
+  		w.loadJS = loadJS;
+  	}
+  }( typeof global !== "undefined" ? global : this ));
+  </script>
   </head>
   <body id="oc-container" class="oc-container">
     <a id="skip-to-content" href="#content">skip to main content</a>
