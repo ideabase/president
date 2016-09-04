f0c133c97ecf20c8f19cbb96c386775f521d7c4f
Christopher Hallahan <challahan@gmail.com>
2016-09-03 22:13:42 -0400

    Adjusted display for video
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/assets/js/script.js
+++ dev/assets/js/script.js
@@ -1749,3 +1749,28 @@ WebFontConfig = {
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(wf, s);
   })();
+
+
+// Pause/Play Embedded Video for Accessibility //
+
+var vid = document.getElementById("bgvid"),
+pauseButton = document.getElementById("vidpause");
+function vidFade() {
+    vid.classList.add("stopfade");
+}
+vid.addEventListener('ended', function() {
+    // only functional if "loop" is removed
+     vid.pause();
+	// to capture IE10
+	vidFade();
+});
+pauseButton.addEventListener("click", function() {
+    vid.classList.toggle("stopfade");
+	if (vid.paused) {
+vid.play();
+		pauseButton.innerHTML = "Pause";
+	} else {
+        vid.pause();
+        pauseButton.innerHTML = "Paused";
+	}
+})
--- dev/prototype/patterns/partials/priority-header.php
+++ dev/prototype/patterns/partials/priority-header.php
@@ -1,9 +1,10 @@
 <header class="priority-header">
   <?php include("../patterns/partials/quotation.html") ?>
   <h2 class="priority-header__heading"><span>1</span>Students First</h2>
-  <video loop muted autoplay poster="../../assets/img/poster.png" class="priority-header__video">
+  <video playsinline loop muted autoplay poster="../../assets/img/fountain-poster.jpg" class="priority-header__video" id="bgvid">
         <!--<source src="video/big_buck_bunny.webm" type="video/webm">-->
-        <source src="../../assets/video/fountain.mp4" type="video/mp4">
+        <source src="../../assets/video/fountain-small.mp4" type="video/mp4">
         <!--<source src="video/big_buck_bunny.ogv" type="video/ogg">-->
   </video>
+  <button class="button-pause" id="vidpause">Pause</button>
 </header>
