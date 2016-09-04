9ff3db434fa1a8b5277bfea58117889b5cf8eb08
Christopher Hallahan <challahan@gmail.com>
2016-09-04 00:47:58 -0400

    Added favicon, adjusted arrows on carousel, adjusted colors for accessibility
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/prototype/patterns/partials/priority-header.php
+++ dev/prototype/patterns/partials/priority-header.php
@@ -7,3 +7,28 @@
   </video>
   <button class="button-pause" id="vidpause">Pause</button>
 </header>
+<script>
+// Pause/Play Embedded Video for Accessibility //
+
+var vid = document.getElementById("bgvid"),
+pauseButton = document.getElementById("vidpause");
+function vidFade() {
+    vid.classList.add("stopfade");
+}
+vid.addEventListener('ended', function() {
+    // only functional if "loop" is removed
+     vid.pause();
+	// to capture IE10
+	vidFade();
+});
+pauseButton.addEventListener("click", function() {
+    vid.classList.toggle("stopfade");
+	if (vid.paused) {
+vid.play();
+		pauseButton.innerHTML = "Pause";
+	} else {
+        vid.pause();
+        pauseButton.innerHTML = "Paused";
+	}
+})
+</script>
