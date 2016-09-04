a49398d3d65098bbecf28ff4c3be483d1471839b
Christopher Hallahan <challahan@gmail.com>
2016-09-03 22:49:31 -0400

    Added webm version of video
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/prototype/patterns/partials/priority-header.php
+++ dev/prototype/patterns/partials/priority-header.php
@@ -2,9 +2,8 @@
   <?php include("../patterns/partials/quotation.html") ?>
   <h2 class="priority-header__heading"><span>1</span>Students First</h2>
   <video playsinline loop muted autoplay poster="../../assets/img/fountain-poster.jpg" class="priority-header__video" id="bgvid">
-        <!--<source src="video/big_buck_bunny.webm" type="video/webm">-->
+        <source src="../../assets/video/fountain-small.webm" type="video/webm">
         <source src="../../assets/video/fountain-small.mp4" type="video/mp4">
-        <!--<source src="video/big_buck_bunny.ogv" type="video/ogg">-->
   </video>
   <button class="button-pause" id="vidpause">Pause</button>
 </header>
