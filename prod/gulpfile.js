7fa1bfcb191395f3ba11a61d5ebcbfd7ab999ae5
Christopher Hallahan <challahan@gmail.com>
2016-09-12 00:16:23 -0400

    Added gulp resize folders
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- prod/gulpfile.js
+++ prod/gulpfile.js
@@ -1,5 +1,40 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var imageResize = require('gulp-image-resize');
+var changed = require("gulp-changed");
+
+// Gallery Crops //
+
+gulp.task('gallery', function () {
+  gulp.src('assets/img/gallery/*.{png,gif,jpg,JPG,JPEG}')
+    .pipe(changed('assets/img/gallery/crop/'))
+    .pipe(imageResize({
+      width : 100,
+      height : 100,
+      crop : true,
+      upscale : false,
+      quality : .8,
+      format : "jpg"
+    }))
+    .pipe(gulp.dest('assets/img/gallery/crop/'));
 });
+
+// Thumbnail Crops //
+
+gulp.task('thumbnail', function () {
+  gulp.src('assets/img/thumbnail/*.{png,gif,jpg,JPG,JPEG}')
+    .pipe(changed('assets/img/thumbnail/crop/'))
+    .pipe(imageResize({
+      width : 500,
+      height : 500,
+      crop : true,
+      upscale : false,
+      quality : .8,
+      format : "jpg"
+    }))
+    .pipe(gulp.dest('assets/img/thumbnail/crop/'));
+});
+
+// Add Task Names Here //
+
+gulp.task('default', ['gallery', 'thumbnail']);
